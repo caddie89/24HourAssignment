38beb443cd3a296230bd7e590fa29d3d0f7ce893
joshuawright89 <73801906+joshuawright89@users.noreply.github.com>
2021-02-06 12:48:31 -0500

    Working on controller

---
+++ 24HourAssignment.WebAPI/Controllers/ReplyController.cs
@@ -0,0 +1,46 @@
+﻿using _24HourAssignment.Models;
+using _24HourAssignment.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24HourAssignment.WebAPI.Controllers
+{
+    [Authorize]
+    public class ReplyController : ApiController
+    {
+        //"Now let's add a Get All method:"
+        public IHttpActionResult Get()
+        {
+        ReplyService replyService = CreateReplyService();
+        var replies = replyService.GetReplies();
+        return Ok(replies);
+        }
+
+        //"And a Post(ReplyCreate reply) method:"
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+
+            return Ok;
+        }
+
+
+        private ReplyService CreateReplyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetCommentId());
+            var replyService = new ReplyService(commentId);
+            return replyService;
+        }
+
+    }
+}
