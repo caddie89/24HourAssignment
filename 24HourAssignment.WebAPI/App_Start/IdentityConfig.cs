405bbe68bff92cc065cb96a1687af63c70012624
Caleb Addie <cjaddie.89@gmail.com>
2021-02-05 23:48:56 -0500

    no errors

--- 24HourAssignment.WebAPI/App_Start/IdentityConfig.cs
+++ 24HourAssignment.WebAPI/App_Start/IdentityConfig.cs
@@ -1,9 +1,6 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
-using _24HourAssignment.WebAPI.Models;
 using _24HourAssignment.Data;
 
 namespace _24HourAssignment.WebAPI
